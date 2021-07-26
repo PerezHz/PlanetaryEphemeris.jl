eaf78c740082ea02db733393f65cfab21a590684
Jorge A. Pérez-Hernández <jperez@icf.unam.mx>
2021-07-25 20:56:08 -0500

    Add integrate_ephemeris.jl script

--- scripts/integrate_ephemeris.jl
+++ scripts/integrate_ephemeris.jl
@@ -16,10 +16,10 @@ const jd0 = datetime2julian(DateTime(1969,6,28,0,0,0)) #starting time of integra
 const dense = true #false
 const dynamics = DE430!
 @show dynamics
-const nast = 16 #343 #25 # number of asteroid perturbers
+const nast = 343 #16 # number of asteroid perturbers
 const quadmath = false #true # use quadruple precision
 ###const bodyind = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 17, 18, 19, 20, 21, 22, 23, 25, 27, 28, 30, 40, 41, 46, 55, 62, 73, 113, 115, 277, 322] # SS + 25 ast perturbers
-const bodyind = 1:(11+nast) # body indices in output
+const bodyind = 1:(11+16) #1:(11+nast) # body indices in output
 
 # integration parameters
 const order = 25
@@ -29,14 +29,6 @@ const abstol = 1.0E-20
 PlanetaryEphemeris.propagate(1, jd0, nyears, output=false, dense=dense, dynamics=dynamics, nast=nast, quadmath=quadmath, bodyind=bodyind, order=order, abstol=abstol)
 println("*** Finished warmup")
 
-# PlanetaryEphemeris.propagate(2, jd0, nyears, output=false, dense=dense, dynamics=dynamics, nast=nast, quadmath=quadmath, bodyind=bodyind, order=order, abstol=abstol)
-# println("*** Finished 2 steps")
-
-# PlanetaryEphemeris.propagate(3, jd0, nyears, output=false, dense=dense, dynamics=dynamics, nast=nast, quadmath=quadmath, bodyind=bodyind, order=order, abstol=abstol)
-# println("*** Finished 3 steps")
-
-PlanetaryEphemeris.propagate(3, jd0, nyears, dense=dense, dynamics=dynamics, nast=nast, quadmath=quadmath, bodyind=bodyind, order=order, abstol=abstol)
-println("*** Finished 3 steps with output file")
-
-#PlanetaryEphemeris.propagate(maxsteps, jd0, nyears, dense=dense, dynamics=dynamics, nast=nast, quadmath=quadmath, bodyind=bodyind, order=order, abstol=abstol)
-#println("*** Finished full integration")
+# perform full integration
+PlanetaryEphemeris.propagate(maxsteps, jd0, nyears, dense=dense, dynamics=dynamics, nast=nast, quadmath=quadmath, bodyind=bodyind, order=order, abstol=abstol)
+println("*** Finished full integration")
