b8c264e5677b8b626d9ea467eaebf4f8d6e26143
Jorge A. Pérez-Hernández <jperez@icf.unam.mx>
2021-07-28 23:58:33 -0500

    Update integrate_ephemeris.jl

--- scripts/integrate_ephemeris.jl
+++ scripts/integrate_ephemeris.jl
@@ -1,7 +1,7 @@
 #Multi-threaded:
-#JULIA_NUM_THREADS=<number-of-threads> julia --project=@. main.jl
+#JULIA_NUM_THREADS=<number-of-threads> julia --project=@. integrate_ephemeris.jl
 #Single-threaded:
-#julia --project=@. main.jl
+#julia --project=@. integrate_ephemeris.jl
 
 @show Threads.nthreads()
 
@@ -9,21 +9,22 @@ using PlanetaryEphemeris
 using Dates, JLD
 
 #script parameters (TODO: use ArgParse.jl instead)
-const maxsteps = 100000
-const nyears = 100.0
-const jd0 = datetime2julian(DateTime(1969,6,28,0,0,0)) #starting time of integration
+maxsteps = 1000000
+nyears = 100.0
+# jd0 = datetime2julian(DateTime(1969,6,28,0,0,0)) #starting time of integration
+jd0 = datetime2julian(DateTime(2000,1,1,12)) #starting time of integration
 @show jd0, J2000, jd0-J2000
-const dense = true #false
-const dynamics = DE430!
+dense = true #false
+dynamics = DE430!
 @show dynamics
-const nast = 343 #16 # number of asteroid perturbers
-const quadmath = false #true # use quadruple precision
-###const bodyind = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 17, 18, 19, 20, 21, 22, 23, 25, 27, 28, 30, 40, 41, 46, 55, 62, 73, 113, 115, 277, 322] # SS + 25 ast perturbers
-const bodyind = 1:(11+16) #1:(11+nast) # body indices in output
+nast = 343 #16 # number of asteroid perturbers
+quadmath = false #true # use quadruple precision
+###bodyind = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 17, 18, 19, 20, 21, 22, 23, 25, 27, 28, 30, 40, 41, 46, 55, 62, 73, 113, 115, 277, 322] # SS + 25 ast perturbers
+bodyind = 1:(11+16) #1:(11+nast) # body indices in output
 
 # integration parameters
-const order = 25
-const abstol = 1.0E-20
+order = 25
+abstol = 1.0E-20
 
 #integrator warmup
 PlanetaryEphemeris.propagate(1, jd0, nyears, output=false, dense=dense, dynamics=dynamics, nast=nast, quadmath=quadmath, bodyind=bodyind, order=order, abstol=abstol)
