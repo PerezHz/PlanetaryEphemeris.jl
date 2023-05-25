322bfc111d4f837c4956537d10104dfba72bb6b0
Jorge A. Pérez Hernández <perez.hz@gmail.com>
2023-05-25 22:47:01 +0200

    Address review

--- src/precompile.jl
+++ src/precompile.jl
@@ -2,15 +2,15 @@ using PrecompileTools
 
 @setup_workload begin
     # Maximum number of steps
-    maxsteps = 2
+    const maxsteps = 2
     # Starting time of integration
-    jd0 = datetime2julian(DateTime(2000,1,1,12)) 
-    # Number of years 
-    nyears = 2031.0 - year(julian2datetime(jd0))
+    const jd0 = datetime2julian(DateTime(2000,1,1,12))
+    # Number of years
+    const nyears = 2031.0 - year(julian2datetime(jd0))
     # Dense output
-    dense = Val(true)
-    # Dynamical function 
-    dynamics = DE430!
+    const dense = Val(true)
+    # Dynamical function
+    const dynamics = DE430!
     @compile_workload begin
         # All calls in this block will be precompiled, regardless of whether
         # they belong to your package or not (on Julia 1.8 and higher)
