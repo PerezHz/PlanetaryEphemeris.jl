0e4123d09f2a6bc85fc38470b9a27d35e3d2447d
Jorge A. Pérez Hernández <perez.hz@gmail.com>
2023-05-27 14:04:52 +0200

    Try running tests using two threads

--- test/propagation.jl
+++ test/propagation.jl
@@ -16,7 +16,7 @@ using PlanetaryEphemeris: initialcond, ssic_1969, ssic_2000, astic_1969, astic_2
     local N_ast = 343
     # Total number of bodies (solar system + asteroids)
     local N = 11 + N_ast
-    # Check initial conditions global variables 
+    # Check initial conditions global variables
     @test size(ssic_1969) == (11, 6)
     @test size(ssic_2000) == (11, 6)
 
@@ -29,7 +29,7 @@ using PlanetaryEphemeris: initialcond, ssic_1969, ssic_2000, astic_1969, astic_2
     @test isa(astic_1969, Matrix{Float64})
     @test isa(astic_2000, Matrix{Float64})
 
-    # Number of bodies 
+    # Number of bodies
     local N = 11 + 343
     # Check output of initialcond
     for jd0 in datetime2julian.( ( DateTime(1969,6,28,0,0,0), DateTime(2000,1,1,12) ) )
@@ -49,39 +49,41 @@ end
 using PlanetaryEphemeris: jd0, nyears, dense, dynamics, order, abstol
 
 @testset "Propagation" begin
-    
+
+    @show Threads.nthreads()
+
     # Number of asteroids
     local N_ast = 343
     # Total number of bodies (solar system + asteroids)
     local N = 11 + N_ast
-    # Indices of bodies to be saved 
+    # Indices of bodies to be saved
     local bodyind = 1:(11+16)
-    # Years to be integrated 
+    # Years to be integrated
     local nyears = 31
 
-    # Float64 
+    # Float64
 
-    # Test integration 
+    # Test integration
     sol64 = propagate(1, jd0, nyears, dense; dynamics = dynamics, order = order, abstol = abstol)
-    # Save solution 
+    # Save solution
     filename = selecteph2jld2(sol64, bodyind, nyears)
-    # Recovered solution 
+    # Recovered solution
     recovered_sol64 = JLD2.load(filename, "ss16ast_eph")
-    # Remove file 
+    # Remove file
     rm(filename)
-    
+
     @test selecteph(sol64, bodyind, euler = true, ttmtdb = true) == recovered_sol64
 
     # Float 128
 
-    # Test integration 
+    # Test integration
     sol128 = propagate(1, Float128(jd0), nyears, dense; dynamics = dynamics, order = order, abstol = abstol)
-    # Save solution 
+    # Save solution
     filename = selecteph2jld2(sol128, bodyind, nyears)
-    # Recovered solution 
+    # Recovered solution
     recovered_sol128 = JLD2.load(filename, "ss16ast_eph")
-    # Remove file 
+    # Remove file
     rm(filename)
-    
+
     @test selecteph(sol128, bodyind, euler = true, ttmtdb = true) == recovered_sol128
 end
