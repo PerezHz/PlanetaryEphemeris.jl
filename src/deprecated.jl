74732e34edc9c8c21d6aa6e815acb9cd2f0606d8
Jorge A. Pérez-Hernández <perez.hz@gmail.com>
2024-08-03 20:41:17 +0200

    Update error message in deprecated method

--- src/deprecated.jl
+++ src/deprecated.jl
@@ -2,4 +2,4 @@ Base.@deprecate propagate(maxsteps, jd0, tspan, ::Val{true}; dynamics = NBP_pN_A
                    nast = 343, order = order, abstol = abstol, parse_eqs = true) propagate(maxsteps, jd0, tspan; dynamics = NBP_pN_A_J23E_J23M_J2S_threads!,
                    nast = 343, order = order, abstol = abstol, parse_eqs = true)
 Base.@deprecate propagate(maxsteps, jd0, tspan, ::Val{false}; dynamics = NBP_pN_A_J23E_J23M_J2S_threads!,
-                   nast = 343, order = order, abstol = abstol, parse_eqs = true) error("PlanetaryEphemeris.propagate always returns dense output.")
+                   nast = 343, order = order, abstol = abstol, parse_eqs = true) error("PlanetaryEphemeris.propagate may only be called with `Val(true)` in the fourth argument.")
