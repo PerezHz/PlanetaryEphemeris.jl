18df3e6ac67a1e0851d75a18e2857a2ffc9d8218
Jorge A. Pérez-Hernández <perez.hz@gmail.com>
2024-08-03 14:51:53 +0200

    Add src/deprecated.jl

---
+++ src/deprecated.jl
@@ -0,0 +1,5 @@
+Base.@deprecate propagate(maxsteps, jd0, tspan, ::Val{true}; dynamics = NBP_pN_A_J23E_J23M_J2S_threads!,
+                   nast = 343, order = order, abstol = abstol, parse_eqs = true) propagate(maxsteps, jd0, tspan; dynamics = NBP_pN_A_J23E_J23M_J2S_threads!,
+                   nast = 343, order = order, abstol = abstol, parse_eqs = true)
+Base.@deprecate propagate(maxsteps, jd0, tspan, ::Val{false}; dynamics = NBP_pN_A_J23E_J23M_J2S_threads!,
+                   nast = 343, order = order, abstol = abstol, parse_eqs = true) error("PlanetaryEphemeris.propagate always returns dense output.")
